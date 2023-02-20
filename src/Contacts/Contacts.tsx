94ae8d66521f909bac85796fe9ad4630b7d6e3e5
Alexandr <alexkazakqq@gmail.com>
2023-02-20 12:07:14 +0300

    Protfolio (part 3) with send message

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./Contacts.module.scss"
 import {Title} from "../common/components/Title/Title";
+import {useFormik} from "formik";
+import axios from "axios";
 const Fade = require("react-reveal/Fade");
 
 export const Contacts = () => {
-    return (
 
+    const [isSending, setIsSending] = useState(false)
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+        },
+        onSubmit: async values => {
+            setIsSending(true)
+            await axios.post('https://gmail-from-portfolio.vercel.app/sendMessage', {name: values.name, email: values.email, message: values.message})
+            alert('Your message is send')
+            setIsSending(false)
+            formik.resetForm()
+        },
+    })
+
+    return (
         <div id={'contacts'} className={s.contactsBlock}>
             <Fade>
                 <div className={s.contactsContainer}>
                     <Title text={"Contacts"}/>
                     <Fade left>
-                        <form className={s.form}>
-                            <input placeholder={"Name"} type={"text"} className={s.formArea}/>
-                            <input placeholder={"e-mail"} type={"text"} className={s.formArea}/>
-                            <textarea placeholder={"Message"} className={s.messageArea}></textarea>
-                            <button type={"submit"}>Send</button>
+                        <form className={s.form} onSubmit={formik.handleSubmit} >
+                            <input placeholder={"Name"} type={"text"} className={s.formArea} {...formik.getFieldProps("name")}/>
+                            <input placeholder={"e-mail"} type={"text"} className={s.formArea} {...formik.getFieldProps("email")}/>
+                            <textarea placeholder={"Message"} className={s.messageArea} {...formik.getFieldProps("message")}></textarea>
+                            <button type={"submit"} disabled={isSending}>Send</button>
                         </form>
                     </Fade>
                 </div>
